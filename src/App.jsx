8742c12004273fb67f84dd0ef414afb4778c264b
gaspym97 <angelgasparmiralles@gmail.com>
2025-12-26 18:13:57 -0500

    footer mobile version created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
   
 
   return (
     <>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
+      <div className="min-h-screen bg-gray-50 flex flex-col">
+        <main className="flex-grow">
+          <Navbar />
+        </main>
+        <Footer />
       </div>
     </>
   )
