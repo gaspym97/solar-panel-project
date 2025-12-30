4e29ef3136b058e2c2a2551c9542ee38060e5783
gaspym97 <angelgasparmiralles@gmail.com>
2025-12-30 12:40:20 -0500

    small change in footer's margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer bottom-0 left-0 w-auto z-50 bg-amber-50 backdrop-blur-md shadow-md shadow-neutral-300 border-amber-200 border rounded-lg p-10 mb-4 m-14">
+    <footer className="footer bottom-0 left-0 w-auto z-50 bg-amber-50 backdrop-blur-md shadow-md shadow-neutral-300 border-amber-200 border rounded-lg p-10 mb-8 m-14">
         <div className="footer__container max-w-7xl mx-auto flex justify-center items-center flex-col space-y-3">
           <p className="footer__text text-amber-800 text-2xl font-bold text-center italic underline">Interested in any of these panels?</p>
           <p className="text-amber-800">Contact us for more details:</p>
